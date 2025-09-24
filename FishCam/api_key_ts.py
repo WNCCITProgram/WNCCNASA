772ea32ba2b297b8b0184c9eceb5eb5093ed1f5a
William Loring <32806514+itinstructor@users.noreply.github.com>
2025-09-24 06:59:59 -0600

    Migrate to new NASA Github

--- FishCam/api_key_ts.py
+++
@@ -1,7 +0,0 @@
-# thingspeak_api_key.py
-# ThingSpeak channel write api key
-
-# GoPiGo Sensor Channel
-THINGSPEAK_API_KEY = 'W98KQBYPETZDIEL4'
-
-THINGSPEAK_URL = 'http://api.thingspeak.com/update'
